9ba21964cec5fdea53395dda7ef9fbec0a4686d9
Student <jsitbon@qcc.cuny.edu>
2019-07-23 17:35:22 -0400

    1st update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const port = 4000;
-
+const mysql = require('mysql');
 //Middleware
 app.use(morgan('dev'));
 app.use(express.json());
@@ -12,11 +12,55 @@ app.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
 });
 
-app.get('/api/students', (req, res) => {
-  const students = [
-    { id: 1, firstName: 'Captain', lastName: 'fancy' },
-    { id: 2, firstName: 'John', lastName: 'buttercup' },
-    { id: 3, firstName: 'Dusty', lastName: 'Trail' },
-  ];
-  res.json(students);
+
+const mysqlConnection = mysql.createConnection({
+host: '10.9.3.218',
+user: 'TWStudent',
+password: 'TechWorks!',
+database: 'employeedb',
+multipleStatements: true
 });
+
+mysqlConnection.connect(err => {
+
+  if(!err)console.log('DB connection succeeded')
+  else{
+    console.log("DB connection failed Error:" + JSON.stringify(err, undefined, 2)
+    );
+
+  }
+});
+
+
+app.get('/employee/:id', (req, res) =>  {
+
+  mysqlConnection.query('SELECT * FROM Employee WHERE EmpID =?', [req.params.id],
+  (err, rows, field) =>{
+
+    if(!err) res.send(rows);
+    else console.log(err);
+  });
+});
+
+app.delete('/employee/:id', (req, res) =>  {
+
+  mysqlConnection.query(
+    'DELETE * FROM Employee WHERE EmpID =?', [req.params.id],
+  (err, rows, field) =>{
+
+    if(!err) res.send("DELETED Employees");
+    else console.log(err);
+  });
+});
+
+
+
+// app.get('/api/students', (req, res) => {
+//   const students = [
+//     { id: 1, firstName: 'Captain', lastName: 'fancy' },
+//     { id: 2, firstName: 'John', lastName: 'buttercup' },
+//     { id: 3, firstName: 'Dusty', lastName: 'Trail' },
+//   ];
+//   res.json(students);
+// });
+
